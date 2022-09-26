6118c8d5811bcdace9e69be646a7b3460792cc2b
woniyoon <gellifisch@gmail.com>
2022-09-26 17:43:15 +0900

    style: apply module css to the SideBar component

--- components/sideBar.js
+++ components/sideBar.js
@@ -1,18 +1,21 @@
 import Link from 'next/link';
+import styles from '@/styles/SideBar.module.css'
 
 export default function SideBar() {
   return (
-    <div style={{flex: 1, height: '100%', border: '1px solid grey', backgroundColor: 'white', flexDirection: "row"}}>
-      <h1>Jae.Y</h1>
-      <Link href="/about">
-        <a>about</a>
-      </Link>
-      <Link href="/works">
-        <a>works</a>
-      </Link>
-      <Link href="/contacts">
-        <a>contacts</a>
-      </Link>
+    <div className={styles.container}>
+      <strong>Jae.Y</strong>
+      <nav className={styles.navMenu}>
+        <Link href="/about">
+          <a style={{marginTop: 5}}>about</a>
+        </Link>
+        <Link href="/works">
+          <a style={{marginTop: 5}}>works</a>
+        </Link>
+        <Link href="/contacts">
+          <a style={{marginTop: 5}}>contacts</a>
+        </Link>
+      </nav>
     </div>
   );
 }
