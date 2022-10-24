3281dcf5f0c4accdc345e6887f65a1618505cd8d
woniyoon <gellifisch@gmail.com>
2022-10-24 16:39:12 +0200

    style: style the timeline menus

---
+++ components/TimelineMenuItem.js
@@ -0,0 +1,17 @@
+import styles from '@/styles/TimelineMenuItem.module.scss'
+
+export default function TimelineMenuItem({item, currentRecordNo, displayThisRecord}) {
+  const {order, name, location, startDate, endDate, skills, description} = item;
+  const isSelected = (order === currentRecordNo);
+
+  const clickHandler = (index) => {
+    displayThisRecord(index);
+  }
+
+  return (
+    <div className={styles.container} onClick={() => {clickHandler(order)}}>
+      <span className={styles.order}>{`${order}.`}</span>
+      <span className={`${styles.dateMenu} ${isSelected ? styles.selectedRecord : ''}`}>{startDate}</span>
+    </div>
+  )
+}
