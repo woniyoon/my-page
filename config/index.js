9acea11af489511798ccd6618ffcff3c14142520
woniyoon <gellifisch@gmail.com>
2022-10-31 15:11:15 +0100

    setting: add Contact DB ID

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 export const DATABASE_ABOUT_ID = process.env.NOTION_ABOUT_DB_ID;
 export const DATABASE_EXP_ID = process.env.NOTION_EXP_DB_ID;
+export const DATABASE_CON_ID = process.env.NOTION_CON_DB_ID;
 export const TOKEN = process.env.NOTION_TOKEN;
