f0b1f6571edf8e88cd437f6d933540ac51a78fca
woniyoon <gellifisch@gmail.com>
2023-02-08 22:12:24 +0900

    misc: add copyright content

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,14 @@
 import NavBar from "./NavBar.js";
 
 export default function Layout({children}) {  // container 개념
+  const copyright = "ⓒ 2023. Jaewon all rights reserved.";
+
   return (
     <div className="container">
       <NavBar />
       <div className='contents-divider'></div>
       <div className='main'>{children}</div>
+      <p className='copyrightTxt'>{copyright}</p>
       <style jsx>{`
         .container {
           background-color: #fff7e6;
@@ -20,6 +23,11 @@ export default function Layout({children}) {  // container 개념
           display: flex;
           justify-content: center;
           flex: 1;
+        }
+        .copyrightTxt {
+          margin: 0.8rem;
+          font-size: 0.8rem;
+          text-align: right;
         }
       `}</style>
     </div>
